4c05c60e6771b5923f41b7cc7c77a29b08c97e0c
Spencer Plant <spencer.plant@shopify.com>
2021-05-05 18:50:54 -0600

    Improve install script

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 
-mydir=$0:A:h
+dotfiles_dir=$0:A:h
 
-do_ohmyzsh () {
+install_oh_my_zsh () {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended  > /dev/null 2>&1
-  cp $mydir/.oh-my-zsh/custom/themes/agnoster.zsh-theme $HOME/.oh-my-zsh/custom/themes
-  cp $mydir/.zshrc $HOME
+  cp $dotfiles_dir/.oh-my-zsh/custom/themes/agnoster.zsh-theme $HOME/.oh-my-zsh/custom/themes
+  cp $dotfiles_dir/.zshrc $HOME
+  source $HOME/.zshrc
 }
 
-do_ohmyzsh
+install_gri () {
+  # Notify if user is not root
+  if [ "$EUID" -ne 0 ]; then 
+    echo "To install gri in /usr/local/bin, your password is needed:"
+  fi
+  sudo cp $dotfiles_dir/gri /usr/local/bin
+}
 
-source $HOME/.zshrc
+install_oh_my_zsh
+install_gri
